1ae54218760d1d1d2e248b6c61627125494b45d9
Ivan <ivangulov@mail.ru>
2022-02-27 02:00:51 +0300

    commit first

---
+++ js/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// console.log("Hello World");
+
+// const result = confirm ("Ты учишь?");
+
+// const answer = prompt("Сколько вам лет?", "30");
+// console. log(ansver);
+
+// const answers = [];
+
+// answers [0] = prompt('Сколько лет тебе', '');
+// answers [1] = prompt('Как тебя зовут?', '');
+// answers [2] = prompt('Какая у тебя фамилия?', '');
+
+// document.write(answers);
+
